052817d2da25a7dcad7591478bbc81de5a9310e4
Andrei Telehoi <andrei.telehoi.99@gmail.com>
2020-04-22 00:51:56 +0300

    'complete'

--- to_do-project/to_do/urls.py
+++ to_do-project/to_do/urls.py
@@ -21,12 +21,18 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Auth
-    path('signup/', views.signup_user, name='signup_user'),
-    path('logout/', views.logout_user, name='logout_user'),
-    path('login/', views.login_user, name='login_user'),
+    path('signup/', views.signupuser, name='signupuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
+    path('login/', views.loginuser, name='loginuser'),
 
     # Todos
-    path('current/', views.current_todos, name='current_todos'),
     path('', views.home, name='home'),
-    path('create', views.create_todo, name='create_todo'),
+    path('current/', views.currenttodos, name='currenttodos'),
+    path('completed/', views.completedtodos, name='completedtodos'),
+    path('create/', views.createtodo, name='createtodo'),
+    path('todo/<int:todo_pk>/', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete',
+         views.completetodo, name='completetodo'),
+    path('todo/<int:todo_pk>/delete',
+         views.deletetodo, name='deletetodo'),
 ]
--- to_do-project/to_do_app/views.py
+++ to_do-project/to_do_app/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
 from .forms import TodoForm
+from .models import Todo
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -12,9 +15,9 @@ def home(req):
     return render(req, 'todo/home.html')
 
 
-def signup_user(req):
+def signupuser(req):
     if req.method == "GET":
-        return render(req, 'todo/signup_user.html', {'form': UserCreationForm()})
+        return render(req, 'todo/signupuser.html', {'form': UserCreationForm()})
     else:
         if req.POST['password1'] == req.POST['password2']:
             try:
@@ -22,51 +25,98 @@ def signup_user(req):
                     req.POST['username'], password=req.POST['password1'])
                 user.save()
                 login(req, user)
-                return redirect('current_todos')
+                return redirect('currenttodos')
             except IntegrityError:
-                return render(req, 'todo/signup_user.html', {
+                return render(req, 'todo/signupuser.html', {
                     'form': UserCreationForm(),
                     'error': 'username already taken'
                 })
         else:
-            return render(req, 'todo/signup_user.html', {
+            return render(req, 'todo/signupuser.html', {
                 'form': UserCreationForm(),
                 'error': 'Passwords did not match'
             })
 
 
-def login_user(req):
+def loginuser(req):
     if req.method == "GET":
-        return render(req, 'todo/login_user.html', {'form': AuthenticationForm()})
+        return render(req, 'todo/loginuser.html', {'form': AuthenticationForm()})
     else:
         user = authenticate(
             req, username=req.POST['username'], password=req.POST['password'])
         if user is None:
-            return render(req, 'todo/login_user.html', {'form': AuthenticationForm(), 'error': 'Username and password did not match'})
+            return render(req, 'todo/loginuser.html', {
+                'form': AuthenticationForm(),
+                'error': 'Username and password did not match'
+            })
         else:
             login(req, user)
-            return redirect('current_todos')
+            return redirect('currenttodos')
 
 
-def logout_user(req):
+@login_required
+def logoutuser(req):
     if req.method == 'POST':
         logout(req)
         return redirect('home')
 
 
-def create_todo(req):
+@login_required
+def createtodo(req):
     if req.method == 'GET':
-        return render(req, 'todo/create_todo.html', {'form': TodoForm()})
+        return render(req, 'todo/createtodo.html', {'form': TodoForm()})
     else:
         try:
             form = TodoForm(req.POST)
             new_todo = form.save(commit=False)
             new_todo.user = req.user
             new_todo.save()
-            return redirect('current_todos')
+            return redirect('currenttodos')
         except ValueError:
-            return render(req, 'todo/create_todo.html', {'error': 'Bad data'})
+            return render(req, 'todo/createtodo.html', {'error': 'Bad data'})
 
 
-def current_todos(req):
-    return render(req, 'todo/current_todos.html')
+@login_required
+def currenttodos(req):
+    todos = Todo.objects.filter(user=req.user, date_completed__isnull=True)
+    return render(req, 'todo/currenttodos.html', {'todos': todos})
+
+
+@login_required
+def viewtodo(req, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=req.user)
+    if req.method == 'GET':
+        form = TodoForm(instance=todo)
+        return render(req, 'todo/viewtodo.html', {'todo': todo, 'form': form})
+    else:
+        try:
+            # instance ca sa stie ca facem update
+            form = TodoForm(req.POST, instance=todo)
+            form.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(req, 'todo/viewtodo.html', {'todo': todo, 'form': form, 'error': 'bad info'})
+
+
+@login_required
+def completetodo(req, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=req.user)
+    if req.method == 'POST':
+        todo.date_completed = timezone.now()
+        todo.save()
+        return redirect('currenttodos')
+
+
+@login_required
+def deletetodo(req, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=req.user)
+    if req.method == 'POST':
+        todo.delete()
+        return redirect('currenttodos')
+
+
+@login_required
+def completedtodos(req):
+    todos = Todo.objects.filter(
+        user=req.user, date_completed__isnull=False).order_by('-date_completed')
+    return render(req, 'todo/completedtodos.html', {'todos': todos})
